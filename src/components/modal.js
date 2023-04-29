bd9ccb61e3429d028e9bb5d5728319f2b1436b07
Vasiliy <vasia22012003@gmail.com>
2023-04-29 12:52:18 +0500

    Start writing functionaity

---
+++ src/components/modal.js
@@ -0,0 +1,11 @@
+export const modal = () => {
+  const menuBtn = document.querySelector('.cta__btn');
+
+  // const showModal = () => {
+
+  // };
+
+  menuBtn.addEventListener('click', () => {
+    // Executed code here
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './index.html';
 import './index.scss';
+import { modal } from './components/modal';
+
+document.addEventListener('DOMContentLoaded', () => {
+  modal();
+});
